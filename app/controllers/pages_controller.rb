854f1cd9530cd8ab6fe58f5bafa3db379afb67e2
gibborjames <gibborjames@gmail.com>
2014-04-18 11:48:00 +0800

    spec pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,10 @@
 class PagesController < ApplicationController
   def index
   end
+  def news
+  end
+  def announcement
+  end
+  def about_us
+  end
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,11 +2,31 @@ require 'spec_helper'
 
 describe PagesController do
 
-  describe "GET 'index'" do
-    it "returns http success" do
+  describe "#index" do
+    it "it should return index page" do
       get 'index'
       response.should be_success
     end
   end
 
+  describe "#news" do
+    it "it should return news pages" do
+      get :news
+      response.should be_success
+    end
+  end
+
+  describe "#announcement" do
+    it "it should return announcement pages" do
+      get :announcement
+      response.should be_success
+    end
+  end
+
+  describe "#about_us" do
+    it "it should return about us pages" do
+      get :about_us
+      response.should be_success
+    end
+  end
 end

3993de25d082ba64ca66e72c16824625e0e7cdf7
gibborjames <gibborjames@gmail.com>
2014-04-28 23:10:29 +0800

    implement registration for new users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,16 @@
 class UsersController < ApplicationController
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path
+    else
+      redirect_to registration_path
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:email, :password, :password_confirmation)
+    end
 end
---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe UsersController do
+
+  describe "#create" do
+    let!(:user) { create(:user) }
+
+    it "should create user on registration form" do
+      
+    end
+  end
+end

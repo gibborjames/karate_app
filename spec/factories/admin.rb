c03813db8f3d74725ac6f330a30bc7c49d188ff9
gibborjames <gibborjames@gmail.com>
2014-05-03 18:31:08 +0800

    fix admin factories.

--- spec/factories/admin.rb
+++ spec/factories/admin.rb
@@ -1,6 +1,9 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 FactoryGirl.define do
   factory :admin do
+    firstname 'admin firstname'
+    middlename 'admin middlename'
+    lastname 'admin lastname'
     email 'admin@example.com'
     password 'password'
     password_confirmation 'password'

eb5104d7f671bde9ace78216e5cd8aa73d4bafaf
gibborjames <gibborjames@gmail.com>
2014-05-10 09:21:31 +0800

    comment registration model.

--- app/assets/javascripts/registration_modal.js
+++ app/assets/javascripts/registration_modal.js
@@ -1,21 +1,22 @@
-var RegistrationModal = function() {
-
-  var self = {};
-
-  self = {
-
-    initialize: function() {
-      $(window).load(function() {
-        if ($('#registrationModal').length > 0) {
-          setTimeout(function() {
-            $('#registrationModal').modal('show');
-          }, 500);
-        }
-      });
-    }
-  }
-  return self;
-}
-
-var registrationModal = new RegistrationModal();
-registrationModal.initialize();
+// Comment this for now
+// var RegistrationModal = function() {
+//
+//   var self = {};
+//
+//   self = {
+//
+//     initialize: function() {
+//       $(window).load(function() {
+//         if ($('#registrationModal').length > 0) {
+//           setTimeout(function() {
+//             $('#registrationModal').modal('show');
+//           }, 500);
+//         }
+//       });
+//     }
+//   }
+//   return self;
+// }
+//
+// var registrationModal = new RegistrationModal();
+// registrationModal.initialize();

ffc71d68e57afe1d623dcfc57284246a18d1b3b3
gibborjames <gibborjames@gmail.com>
2014-05-10 12:52:05 +0800

    remove last migration

--- db/migrate/20140510044005_add_column_id_to_users.rb
+++
@@ -1,7 +0,0 @@
-class AddColumnIdToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :personal_info_id, :integer
-    add_column :users, :other_info_id, :integer
-    add_column :users, :case_of_emergency_info_id, :integer
-  end
-end

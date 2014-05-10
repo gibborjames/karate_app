df71320210e019e5e96694a221e5aa6b44a76f1e
gibborjames <gibborjames@gmail.com>
2014-05-10 11:55:54 +0800

    registration form initial completed.

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,12 +1,27 @@
 require 'spec_helper'
 
-describe UsersController do
+describe UsersController, focus: true do
 
   describe "#create" do
-    let!(:user) { create(:user) }
+    context 'with valid parameters' do
+      it 'should create new record of user' do
+        user_params = {
+          user: {
+            email: 'james@example.com',
+            firstname: 'james',
+            middlename: 'umil',
+            lastname: 'delacruz',
+            password: SecureRandom.hex(5)
+          }
+        }
+        expect {
+          post :create, user_params
+        }.to change(User, :count).by(1)
+      end
+    end
 
-    it "should create user on registration form" do
-      
+    context 'with invalid parameters' do
+
     end
   end
 end

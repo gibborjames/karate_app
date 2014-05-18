29c59687e6764b61d04ff75b411cc20da1edc2a1
gibborjames <gibborjames@gmail.com>
2014-05-18 16:19:35 +0800

    update seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,5 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Admin.create(:email => 'admin@example.com', :password => 'password', :password_confirmation => 'password', :admin => true, :firstname => 'firstname', :middlename => 'middlename', :lastname => 'lastname')
-User.create!(:email => 'user@example.com', :password => 'password', :password_confirmation => 'password', :firstname => 'firstname', :middlename => 'middlename', :lastname => 'lastname')
+Admin.create(:email => 'admin@example.com', :password => 'password', :password_confirmation => 'password', :admin => true, :firstname => 'firstname', :middlename => 'middlename', :lastname => 'lastname', :facebook_account => 'www.facebook.com/1213123')
+User.create!(:email => 'user@example.com', :password => 'password', :password_confirmation => 'password', :firstname => 'firstname', :middlename => 'middlename', :lastname => 'lastname', :facebook_account => 'www.facebook.com/1213123')

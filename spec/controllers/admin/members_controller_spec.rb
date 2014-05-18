2d4fba52a4be7b44251debec36357243f1eb49ac
gibborjames <gibborjames@gmail.com>
2014-05-18 17:48:00 +0800

    add test spec for members controller

--- spec/controllers/admin/members_controller_spec.rb
+++ spec/controllers/admin/members_controller_spec.rb
@@ -4,12 +4,23 @@ describe Admin::MembersController do
 
   describe '#index' do
     let(:admin) { create(:admin) }
+    let(:user) { create(:user) }
 
     # needs to add different scenario for this test
-    it "should return members index page" do
-      sign_in admin
-      get :index
-      response.should be_success
+    context "when user is not admin" do
+      it "should redirect_to root_path" do
+        sign_in user
+        get :index
+        response.should redirect_to root_path
+      end
+    end
+
+    context "when user is admin" do
+      it "should return members index page" do
+        sign_in admin
+        get :index
+        response.should be_success
+      end
     end
   end
 end

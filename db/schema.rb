6b1fb548c508809214039f16852ac5bcc73fa67b
gibborjames <gibborjames@gmail.com>
2014-05-24 23:32:45 +0800

    add user_id to PersonalInfo table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140524153020) do
+ActiveRecord::Schema.define(version: 20140524153159) do
 
   create_table "case_of_emergency_infos", force: true do |t|
     t.string   "name"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 20140524153020) do
     t.date     "date_started_in_karate"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|

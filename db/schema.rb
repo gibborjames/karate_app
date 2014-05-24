a7a982212fb7c51905e5364d3ddf06e1b3b2ef68
gibborjames <gibborjames@gmail.com>
2014-05-24 23:29:49 +0800

    add user_id to case of emergency table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140510043347) do
+ActiveRecord::Schema.define(version: 20140524152834) do
 
   create_table "case_of_emergency_infos", force: true do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20140510043347) do
     t.string   "address"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "other_infos", force: true do |t|

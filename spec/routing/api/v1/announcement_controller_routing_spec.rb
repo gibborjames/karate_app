3f59c4768c163b01b9b275cbbee5b80c77899e90
gibborjames <gibborjames@gmail.com>
2014-05-25 12:19:39 +0800

    create routing specs for hide announcement

--- spec/routing/api/v1/announcement_controller_routing_spec.rb
+++ spec/routing/api/v1/announcement_controller_routing_spec.rb
@@ -2,4 +2,5 @@ require 'spec_helper'
 
 describe Api::V1::AnnouncementsController do
   it { put("/api/v1/announcements/1/publish").should route_to("api/v1/announcements#publish", :id => "1") }
+  it { put("/api/v1/announcements/1/hide").should route_to("api/v1/announcements#hide", :id => "1") }
 end

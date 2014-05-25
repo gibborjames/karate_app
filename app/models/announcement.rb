83691da787c6d8598ddb9d96e9f46f5a36e64ee2
gibborjames <gibborjames@gmail.com>
2014-05-25 11:47:55 +0800

    create publish method

--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -4,4 +4,14 @@ class Announcement < ActiveRecord::Base
   validates :description, presence: true
 
   belongs_to :user
+
+  def self.publish(params)
+    announcement = Announcement.find(params[:id])
+    if announcement.user.present?
+      announcement.is_announce = true
+      announcement.save
+    else
+      announcement
+    end
+  end
 end
